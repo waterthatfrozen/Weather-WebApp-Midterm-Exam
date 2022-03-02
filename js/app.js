cc3b9baab9e163c7b537bc00d49958663f6656ae
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2022-03-02 18:21:38 +0700

    add case NOSTRA API invalid credentials

--- js/app.js
+++ js/app.js
@@ -36,60 +36,64 @@ var main = function () {
                 frequency: mode
         },
         success: function (response){
-            //Create data for new chart
-            var weather = response.results.weather;
-            xValues = []; yValues = []; weatherIcons = []; weatherDescription = []; barColor = [];
-            if(mode==="daily"){
-                weather.forEach(element => {
-                    xValues.push(element.timeStamp);
-                    yValues.push(element.temperature.temp);
-                    weatherIcons.push(element.icon);
-                    weatherDescription.push(element.description);
-                    barColor.push("lightblue");
-                });
-            }
-            if(mode==="3hours"){
-                for(var i=0;i<=7;i++){
-                    xValues.push(weather[i].timeStamp);
-                    yValues.push(weather[i].temperature.temp);
-                    weatherIcons.push(weather[i].icon);
-                    weatherDescription.push(weather[i].description);
-                    barColor.push("lightblue");
+            if(response.errorMessage!==null){
+                alert("Nostra API\n"+response.errorMessage);
+            }else{
+                //Create data for new chart
+                var weather = response.results.weather;
+                xValues = []; yValues = []; weatherIcons = []; weatherDescription = []; barColor = [];
+                if(mode==="daily"){
+                    weather.forEach(element => {
+                        xValues.push(element.timeStamp);
+                        yValues.push(element.temperature.temp);
+                        weatherIcons.push(element.icon);
+                        weatherDescription.push(element.description);
+                        barColor.push("lightblue");
+                    });
                 }
+                if(mode==="3hours"){
+                    for(var i=0;i<=7;i++){
+                        xValues.push(weather[i].timeStamp);
+                        yValues.push(weather[i].temperature.temp);
+                        weatherIcons.push(weather[i].icon);
+                        weatherDescription.push(weather[i].description);
+                        barColor.push("lightblue");
+                    }
+                }
+                //Find min and max to change color of bar chart. Min -> green, Max -> red.
+                var minTemp = Math.min.apply(null,yValues);
+                var maxTemp = Math.max.apply(null,yValues);
+                var minTempIndex = yValues.indexOf(minTemp);
+                var maxTempIndex = yValues.indexOf(maxTemp);
+                barColor[minTempIndex] = "lightgreen";
+                barColor[maxTempIndex] = "pink";
+                //Chart Part
+                $(".graph").empty();
+                $(".graph").append($("<canvas id='weather-chart'>").attr("style","width:1000px;max-width:1000px;"));
+                var chart = new Chart("weather-chart", {
+                    type: "bar",
+                    data: {
+                        labels: xValues,
+                        datasets: [{ backgroundColor: barColor, data: yValues}]
+                    },
+                    options: { 
+                        legend: {display: false}, title: {display: false}}
+                });
+                //Weather Icon Part
+                var $weathericon = $(".weather-icon");
+                var margin_right = (mode === "3hours") ? "61px":"76px";
+                var padding_left = (mode === "3hours") ? "3.5rem":"5rem";
+                $weathericon.empty();
+                $weathericon.css("padding-left",padding_left);
+                var descIndex = 0;
+                weatherIcons.forEach(element => {
+                    $weathericon.append($("<img>").attr({src:element, title:weatherDescription[descIndex]}));
+                    descIndex++;
+                });
+                $("img").css("margin-right",margin_right);
             }
-            //Find min and max to change color of bar chart. Min -> green, Max -> red.
-            var minTemp = Math.min.apply(null,yValues);
-            var maxTemp = Math.max.apply(null,yValues);
-            var minTempIndex = yValues.indexOf(minTemp);
-            var maxTempIndex = yValues.indexOf(maxTemp);
-            barColor[minTempIndex] = "lightgreen";
-            barColor[maxTempIndex] = "pink";
-            //Chart Part
-            $(".graph").empty();
-            $(".graph").append($("<canvas id='weather-chart'>").attr("style","width:1000px;max-width:1000px;"));
-            var chart = new Chart("weather-chart", {
-                type: "bar",
-                data: {
-                    labels: xValues,
-                    datasets: [{ backgroundColor: barColor, data: yValues}]
-                },
-                options: { 
-                    legend: {display: false}, title: {display: false}}
-            });
-            //Weather Icon Part
-            var $weathericon = $(".weather-icon");
-            var margin_right = (mode === "3hours") ? "61px":"76px";
-            var padding_left = (mode === "3hours") ? "3.5rem":"5rem";
-            $weathericon.empty();
-            $weathericon.css("padding-left",padding_left);
-            var descIndex = 0;
-            weatherIcons.forEach(element => {
-                $weathericon.append($("<img>").attr({src:element, title:weatherDescription[descIndex]}));
-                descIndex++;
-            });
-            $("img").css("margin-right",margin_right);
         },
-        error: function (response){window.alert(response.errorMessage);}
+            error: function (response){window.alert(response);}
         });
     }
     //Button Event Part
